ba440929dbcba2a6c151751d0b8b302743edbe86
Katarzyna Lenczewska <klenczewsk001@cee.ema.ad.pwcinternal.com>
2018-08-03 14:29:31 +0200

    categorical features added

--- 2_data_preparation/prev_app.py
+++ 2_data_preparation/prev_app.py
@@ -33,7 +33,7 @@ prev_app = prev_app.drop(columns, axis = 1)
 numericCols = ['SK_ID_CURR', 'AMT_ANNUITY','AMT_APPLICATION','AMT_CREDIT','AMT_DOWN_PAYMENT','AMT_GOODS_PRICE','CNT_PAYMENT','DAYS_DECISION','HOUR_APPR_PROCESS_START',
 'RATE_DOWN_PAYMENT','DAYS_TERMINATION','DAYS_LAST_DUE','DAYS_LAST_DUE_1ST_VERSION','DAYS_FIRST_DUE','DAYS_FIRST_DRAWING']
 dfNumerics = prev_app[numericCols]
-application = f.agg_numeric(dfNumerics, 'SK_ID_CURR', table, application)
+application = agg_numeric(dfNumerics, 'SK_ID_CURR', table, application)
 
 objectCols = ['SK_ID_CURR', 'NAME_CONTRACT_TYPE', 'WEEKDAY_APPR_PROCESS_START','CODE_REJECT_REASON','NAME_TYPE_SUITE',
             'NAME_PAYMENT_TYPE','NAME_CASH_LOAN_PURPOSE']
@@ -51,12 +51,12 @@ prev_app_Approved = prev_app.select_dtypes(include = numerics).drop(columns = ['
 #df.iloc[:,1:].div(df.A, axis=0)
 prev_app_Approved = pd.concat([prev_app_Approved.SK_ID_CURR, prev_app_Approved.iloc[:, 1:].multiply(Approved, axis = 0)], axis = 1)
 
-application = f.agg_numeric(prev_app_Approved, 'SK_ID_CURR', 'prevApp', application)
+application = agg_numeric(prev_app_Approved, 'SK_ID_CURR', 'prevApp', application)
 
 #df representing values for cancelled loans
 prev_app_Canceled = prev_app.select_dtypes(include = numerics).drop(columns = ['SK_ID_PREV'])
 prev_app_Canceled = pd.concat([prev_app_Canceled.SK_ID_CURR, prev_app_Canceled.iloc[:, 1:].multiply(Canceled, axis = 0)], axis = 1)
-application = f.agg_numeric(prev_app_Canceled, 'SK_ID_CURR', 'prevApp', application)
+application = agg_numeric(prev_app_Canceled, 'SK_ID_CURR', 'prevApp', application)
 
 gc.enable()
 del prev_app_Approved, prev_app_Canceled, numerics, Approved, Canceled, dfObjects, objectCols
