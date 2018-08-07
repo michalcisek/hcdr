5b897fb6b205d66ae57d2cc481be2aa08aff8ea1
Katarzyna Lenczewska <klenczewsk001@cee.ema.ad.pwcinternal.com>
2018-08-07 14:28:15 +0200

    features on poshCashBal and creditCardBal data

--- 2_data_preparation/creditCardBal.py
+++ 2_data_preparation/creditCardBal.py
@@ -3,14 +3,18 @@
 Created on Mon Aug  6 16:10:49 2018
 
 @author: klenczewsk001
+
+-- pyt co to jest AMT_BALANCE - zgodnie z internetem to 
+mowi to o wykorzystaniu dostepnego limitu, czyli ile było pociagniec w danym miesiacu
+-- pokazac ze nie ma sensu podwojne grupowanie, zazwyczaj tylko 1 prev kred per curr_id
+-- co oznaczaja te wszystkie charakterystyki dot receivable
 """
 
 import pandas as pd
-import numpy as np
 import pyarrow.feather as feather
 import gc
 
-table = ''
+table = 'credCardBal'
 app_train = pd.read_csv(r'1_data_import\application_train.csv')
 app_test = pd.read_csv(r'1_data_import\application_test.csv')
 credCardBal = pd.read_csv(r'1_data_import\credit_card_balance.csv')
@@ -30,5 +34,30 @@ credCardBal['DrawingsToLimit'] = (credCardBal.AMT_DRAWINGS_ATM_CURRENT +
            
 credCardBal['MinInstToLimit'] = credCardBal.AMT_INST_MIN_REGULARITY/credCardBal.AMT_CREDIT_LIMIT_ACTUAL
 
+credCardBal['ReceiveablePrincipalToTotal'] = credCardBal.AMT_RECEIVABLE_PRINCIPAL/credCardBal.AMT_TOTAL_RECEIVABLE
 
-credCardBal.head()
+credCardBal['TotalPaymentToMinInst'] = credCardBal.AMT_PAYMENT_TOTAL_CURRENT/credCardBal.AMT_INST_MIN_REGULARITY
+
+credCardBal['AmtPerATMDrawing'] = credCardBal.AMT_DRAWINGS_ATM_CURRENT/credCardBal.CNT_DRAWINGS_ATM_CURRENT
+
+credCardBal['AmtPerCurrentDrawing'] = credCardBal.AMT_DRAWINGS_CURRENT/credCardBal.CNT_DRAWINGS_CURRENT
+credCardBal['AmtPerOtherDrawing'] = credCardBal.AMT_DRAWINGS_OTHER_CURRENT/credCardBal.CNT_DRAWINGS_OTHER_CURRENT
+credCardBal['AmtPerPOSDrawing'] = credCardBal.AMT_DRAWINGS_POS_CURRENT/credCardBal.CNT_DRAWINGS_POS_CURRENT
+
+agg = credCardBal.drop(columns = ['SK_ID_PREV', 'CNT_INSTALMENT_MATURE_CUM']).groupby(['SK_ID_CURR']).agg(['mean', 'min', 'max','sum','var','std'])
+
+columns = []
+for var in agg.columns.levels[0]:
+    for stat in agg.columns.levels[1]:
+        columns.append('%s_%s_%s' % (table, var, stat))
+        
+agg.columns = columns
+agg['SK_ID_CURR'] = agg.index
+
+application = application.merge(agg, on = 'SK_ID_CURR', how = 'left')
+
+gc.enable()
+del table, app_train, app_test, credCardBal, df
+gc.collect()
+
+feather.write_feather(application, 'credCardBal_features')
---
+++ 2_data_preparation/poshCash_bal.py
@@ -0,0 +1,44 @@
+'''
+TODO: 
+    - na jakich rachunkach liczymy zmienne? tylko ze statusem Active?
+    - 
+    - 
+'''
+import pandas as pd
+import pyarrow.feather as feather
+
+table = 'poshCashBal'
+app_train = pd.read_csv(r'1_data_import\application_train.csv')
+app_test = pd.read_csv(r'1_data_import\application_test.csv')
+poshCash_bal = pd.read_csv(r'1_data_import\POS_CASH_balance.csv')
+df = app_train.append(app_test).reset_index()
+
+application = df[['SK_ID_CURR']]
+
+poshCash_bal = poshCash_bal[poshCash_bal.NAME_CONTRACT_STATUS == 'Active']
+numerics = ['int64', 'float64']
+poshCash_bal = poshCash_bal.select_dtypes(include = numerics).drop(columns = ['MONTHS_BALANCE'])
+#spr na 1 ekspozycji
+#test_df = poshCash_bal[poshCash_bal.SK_ID_CURR == 334279].select_dtypes(include = ['int64'])
+
+agg1 = poshCash_bal.groupby(['SK_ID_CURR', 'SK_ID_PREV']).agg(['mean', 'min', 'max','sum','var','std'])
+columns = []
+for var in agg1.columns.levels[0]:
+    for stat in agg1.columns.levels[1]:
+        columns.append('%s_%s_%s' % (table, var, stat))
+
+agg1.columns = columns
+
+agg2 = agg1.groupby(['SK_ID_CURR']).agg(['mean', 'min', 'max','sum','var','std'])
+
+columns = []
+for var in agg2.columns.levels[0]:
+    for stat in agg2.columns.levels[1]:
+        columns.append('%s_%s' % (var, stat))
+
+agg2.columns = columns
+agg2['SK_ID_CURR'] = agg2.index
+
+application = application.merge(agg2, on = 'SK_ID_CURR', how = 'left')
+
+feather.write_feather(application, 'posCashBal_features')
